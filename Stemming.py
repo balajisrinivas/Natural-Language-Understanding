55c3d7f34d6e9fd82af7f4834221617b89679bba
Balaji S <39753699+balajisrinivas@users.noreply.github.com>
2020-04-24 17:31:39 +0530

    Update Stemming.py

--- Stemming.py
+++ Stemming.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan  1 20:40:19 2019
-
-@author: Krish.Naik
-"""
-
 import nltk
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
@@ -41,13 +34,5 @@ stemmer = PorterStemmer()
 for i in range(len(sentences)):
     words = nltk.word_tokenize(sentences[i])
     words = [stemmer.stem(word) for word in words if word not in set(stopwords.words('english'))]
-    sentences[i] = ' '.join(words)   
-    
-    
-    
-    
-    
-    
-    
-    
+    sentences[i] = ' '.join(words)
     
